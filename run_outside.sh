ed585fc4e241f32d215087c545ff6ceebbc9d88b
xuanbo.chen <xuanbo.chen@ailabs.tw>
2024-09-02 00:16:32 +0800

    feat: make external monitor rounded showing

--- run_outside.sh
+++ run_outside.sh
@@ -2,13 +2,13 @@
 
 PROJECT="/Users/mac/Documents/hsemotion-onnx"
 # PROJECT="/Users/shanboy/Documents/project/hsemotion-onnx"
-RTSP_URL="rtsp://admin:kris0226@192.168.1.101:554/cam/realmonitor?channel=1&subtype=0 rtsp://admin:kris0226@192.168.1.100:554/cam/realmonitor?channel=1&subtype=0"
+RTSP_URL1="rtsp://admin:kris0226@192.168.1.100:554/cam/realmonitor?channel=1&subtype=0"
+RTSP_URL2="rtsp://admin:kris0226@192.168.1.101:554/cam/realmonitor?channel=1&subtype=0"
 
-export SADNESS_OFFSET=0.0
+export SADNESS_OFFSET=0.55
 export EMOTION_TO_BRANCH=1
 
 cd $PROJECT
 source py312/bin/activate \
 && cd hsemotion_onnx \
-&& python facial_emotions_demo.py --skip-frame 13 --rtsp-url $RTSP_URL
-# && python facial_emotions_demo.py 
+&& python facial_emotions_demo.py --skip-frame 13 --rtsp-url $RTSP_URL1 $RTSP_URL2
