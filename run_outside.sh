9299ecb56ebc733a26869a4ca31434d6ef0c5c39
莊向峰 <gotop_mini_m2@zhuangxiangfengdeMac-mini.local>
2024-09-02 00:17:51 +0800

    fix: cleanup external monitor

--- run_outside.sh
+++ run_outside.sh
@@ -1,14 +1,27 @@
 #/bin/bash
 
-PROJECT="/Users/mac/Documents/hsemotion-onnx"
+PROJECT="/Users/gotop_mini_m2/hsemotion-onnx"
 # PROJECT="/Users/shanboy/Documents/project/hsemotion-onnx"
-RTSP_URL="rtsp://admin:kris0226@192.168.1.101:554/cam/realmonitor?channel=1&subtype=0"
+RTSP_URL="rtsp://admin:kris0226@192.168.1.100:554/cam/realmonitor?channel=1&subtype=0"
+# RTSP_URL="rtsp://97.68.104.34/axis-media/media.amp"
 
-export SADNESS_OFFSET=0.0
+export SADNESS_OFFSET=-0.55
 export EMOTION_TO_BRANCH=1
 
+# Function to clean up background processes
+cleanup() {
+    echo "Cleaning up..."
+    pkill -f "python"      # Kills the Python process
+    echo "Done."
+}
+
+# Ensure cleanup is called in the beginning
+cleanup
+
 cd $PROJECT
-source py312/bin/activate \
+source py39/bin/activate \
 && cd hsemotion_onnx \
-&& python facial_emotions_demo.py --skip-frame 13 --rtsp-url $RTSP_URL
+&& python facial_emotions_demo.py --skip-frame 10 --rtsp-url $RTSP_URL
 # && python facial_emotions_demo.py 
+
+cleanup
